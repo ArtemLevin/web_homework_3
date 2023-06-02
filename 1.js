5ed6bba31e45153a637334003d5f574089cc74d4
ArtemLevin <python_study_2022@mail.ru>
2023-06-02 17:44:44 +0300

    init commit

---
+++ 1.js
@@ -0,0 +1,4 @@
+let degrees = parseInt(prompt("Enter degree"))
+alert("Degrees of Celsium" + " " + "is" + " " + degrees)
+let fahrenheit = degrees*9/5 + 32
+alert("Degrees of Fahrenheit" + " " + "is" + " "  + fahrenheit)
---
+++ 2.js
@@ -0,0 +1,6 @@
+let username = prompt("Enter your name ")
+function greetings(username){
+    alert("Greeting to" + " " + username);
+}
+
+greetings(username);
